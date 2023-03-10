a13f904c33f4649144225f901726e15df0b61864
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-03-10 14:33:49 +0700

    Seminar 8 задача 53 Изменение местами 1 и последней строки

---
+++ Task 53/Program.cs
@@ -0,0 +1,54 @@
+﻿// задача 53. Задайте двумерный массив. Напишите программу которая поменяет первую и последнюю строку массива
+
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void ChangeMatrix(int[,] matr)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        int number = matr[0, j];
+        matr[0, j] = matr[matr.GetLength(0) - 1, j];
+        matr[matr.GetLength(0) - 1, j] = number;
+    } 
+}
+
+
+int m = ReadInt("Введите количетсво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+ChangeMatrix(myMatrix);
+PrintMatrix(myMatrix);
+
