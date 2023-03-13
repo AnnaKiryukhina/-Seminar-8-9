7885853f727c5d6a058d9882abf91827b000f24b
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-03-13 14:40:17 +0700

    Seminar 9 задача 66 решение задачи на сумму чисел в промежутке от  N до M. Рекурсия.

---
+++ DZ Task 66_Sem 9/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.WriteLine("Введите значение М");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите значение N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber (int n, int m)
+{
+    
+    if ( n==m) return m;
+    return m + SumNumber(n, m-1);
+
+}
+
+System.Console.WriteLine(SumNumber(m,n));
